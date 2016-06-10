89f159ff40b52a32011596f482b0efa480cb4d0d
Teacher <Teacher>
2016-06-09 20:29:32 -0400

    added a contact me page

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ server.get('/about', function(request, response){
   response.sendFile('public/html/about.html', {root: __dirname});
 });
 
+server.get('/contact', function(request, response){
+  response.sendFile('public/html/contact.html', {root: __dirname});
+});
+
 server.listen(8080);
